80b86333c0a1495f19921016c86005608a22c84b
SamuelPerezC <samuelperezcontreras@gmail.com>
2026-02-04 20:23:31 -0500

    Delete api_datos_autores.py

--- api_datos_autores.py
+++
@@ -1,8 +0,0 @@
-class Api_lista_autores:
-    def __init__(self):
-        self.Api_lista_autores=[]
-
-    def guardar_autores(self, lista_autor):
-        self.Api_lista_autores.append(lista_autor)
-        print(self.Api_lista_autores)
-
