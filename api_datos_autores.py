2306a988ced7438c4784fa11286b46f4eef69d7f
SamuelPerezC <samuelperezcontreras@gmail.com>
2026-02-05 20:57:26 -0500

    Add files via upload

--- api_datos_autores.py
+++ api_datos_autores.py
@@ -1,19 +1,65 @@
 class Api_lista_autores:
+    #Creamos el constructor
     def __init__(self):
+        #Creamos el atributo
         self.Api_lista_autores=[]
-
+        
+    #METODO APPEND
     def guardar_autores(self, nuevo_autor):
         datos_nuevos = [nuevo_autor.get_nombre(),nuevo_autor.get_fecha()]
         self.Api_lista_autores.append(datos_nuevos)
+        print("Se esta utilizando el metodo Append")
+        
+    #METODO EXTEND
+    def extender_autores(self,lista_autores):
+        self.Api_lista_autores.extend(lista_autores)
+        print("Se esta utilizando el metodo Extend" , self.Api_lista_autores)
+        
+    #METODO INSERT
+    def insertar_autor(self, autor, posicion):
+        datos_autor = [autor.get_nombre(), autor.get_fecha()]
+        self.Api_lista_autores.insert(posicion, datos_autor)
+        print("Se esta utilizando el metodo insert")
+    
+    #METODO REMOVE
+    def remover_autor(self, datos_autor):
+        self.Api_lista_autores.remove(datos_autor)
+        print("Se esta utilizando el metodo Remove")
+        
+    #METODO POP
+    def eliminar_autor(self, posicion):
+        self.Api_lista_autores.pop(posicion)
+        print("Se esta utilizando el metodo Pop")
+
+    #METODO INDEX
+    def buscar_autor(self, datos_autor):
+        return self.Api_lista_autores.index(datos_autor)
+        print("Se esta utilizando el metodo index")
+
+    #METODO COUNT
+    def contar_autor(self, datos_autor):
+        return self.Api_lista_autores.count(datos_autor)
+        print("se esta utilizando el metodo count")
+
+    #METODO SORT
+    def ordenar_autores(self):
+        self.Api_lista_autores.sort()
+        print("se esta utilizando el metodo sort")
+
+    #METODO REVERSE
+    def invertir_autores(self):
+        self.Api_lista_autores.reverse()
+        print("se esta utilizando el metodo reverse")
+
 
     def mostrar_lista_autores(self):
         print(self.Api_lista_autores)
 
         for i in range(len(self.Api_lista_autores)):
-            print("imprime por posicion")
+            print("imprime por posicion MOSTRAMOS LAS FILAS")
             print(self.Api_lista_autores[i])
 
             for j in range (len(self.Api_lista_autores[i])):
-                print("imprime por posicion de las hijas")
+                print("imprime por posicion de las hijas MOSTRAMOS LAS COLUMNAS")
                 print(self.Api_lista_autores[i][j])
 
--- principal.py
+++ principal.py
@@ -8,33 +8,45 @@ obj_bd=base_datos_libro()
 
 #CON ESTE CREAMOS UN AUTOR COMO UN OBJETO
 autor1 = autor("Masculino", "Samuel Perez", "28 años", "14/08/1995")
-autor2 = autor("Femenino", "Kelly Contreras", "25 años", "01/12/1998")
+# autor2 = autor("Femenino", "Kelly Contreras", "25 años", "01/12/1998")
 
-#CON EST SE GUARDA EN LA API
+#CON ESTE SE GUARDA EN LA API
 obj_api_autores.guardar_autores(autor1)
-obj_api_autores.guardar_autores(autor2)
+# obj_api_autores.guardar_autores(autor2)
 
-#CON ESTE MOSTRAMOS LA LISTA
-obj_api_autores.mostrar_lista_autores()
 
 obj_autor=autor("genero masculino", "juan perez", "45 años", "12 de marzo de 1978")
-lista_datos_autor=["Samuel", "Perez", "14/08/1995", "Colombiano"]
-lista_datos_autor1=["Kelly", "Contreras", "01/12/1998", "Colombiana"]
+lista_datos_autor=[["Kelly", "Contreras", "01/12/1998", "Colombiana"], ["Samuel", "Perez", "14/08/1995", "Colombiano"]]
 
 #CREAMOS UN LIBRO COMO OBJETO
 obj_libro1=Libro_modelo("10 de febrero", "250 hojas", "ciencia ficcion", "ficcion")
-obj_libro2=Libro_modelo("12 de febrero", "60 hojas", "ciencia", "Voler al futuro")
-obj_libro3=Libro_modelo("10 de marzo", "50 hojas", "Goku", "DragonBallz")
+# obj_libro2=Libro_modelo("12 de febrero", "60 hojas", "ciencia", "Voler al futuro")
+# obj_libro3=Libro_modelo("10 de marzo", "50 hojas", "Goku", "DragonBallz")
 
-#GUARDAMOS EL LIBRO EN LA BASE DE DATOS
+#GUARDAMOS EL LIBRO EN LA BASE DE DATOS// LLAMAMOS EL METODO APPEND
 obj_bd.guardar_libros(obj_libro1)
-obj_bd.guardar_libros(obj_libro2)
-obj_bd.guardar_libros(obj_libro3)
+# obj_bd.guardar_libros(obj_libro2)
+# obj_bd.guardar_libros(obj_libro3)
 
 #MOSTRAMOS LA INFORMACION DEL BASE DE DATOS
 obj_bd.mostrar_info()
 
 
+#---------
 
+# insert
+obj_api_autores.insertar_autor(autor1, 0)
 
+# sort
+obj_api_autores.ordenar_autores()
 
+# reverse
+obj_api_autores.invertir_autores()
+
+# llamamos el metodo mostrar de api_datos_autores.py
+obj_api_autores.mostrar_lista_autores()
+
+# pop
+obj_api_autores.eliminar_autor(1)
+
+obj_api_autores.extender_autores(lista_datos_autor)
